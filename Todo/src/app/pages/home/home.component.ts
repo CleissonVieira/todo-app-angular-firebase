176b84fcd1eadbde64fdd0e22ed19d9195b23b19
CleissonVieira <cleisson.raimundi@gmail.com>
2022-11-14 10:14:30 -0300

    41%, routes, links, logins, home, card, tabs...

---
+++ Todo/src/app/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  loading = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submit() {
+    this.loading = this.loading ? false : true
+  }
+
+}
