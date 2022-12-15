718b253d09ad49e95ab98edefaa44db0e0024b23
CleissonVieira <cleisson.raimundi@gmail.com>
2022-12-15 17:49:31 -0300

    infor de user e backend

---
+++ frontend/src/app/components/user-card/user-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Component({
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.css']
+})
+export class UserCardComponent implements OnInit {
+
+  user: any = {
+    name: "",
+    picture: "",
+  }
+
+  constructor(
+    private afAuth: AngularFireAuth
+  ) { }
+
+  ngOnInit(): void {
+    this.afAuth.user.subscribe((data) => {
+      this.user.name = data?.displayName;
+      this.user.picture = data?.photoURL;
+    })
+  }
+
+  logout() {
+    this.afAuth.signOut();
+  }
+}
